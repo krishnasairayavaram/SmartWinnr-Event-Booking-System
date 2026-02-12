33c80ab44158306d9b20ef8eeeb3101e4029a405
krishnasairayavaram <krishnasairayavaram2005@gmail.com>
2026-02-12 13:00:10 +0530

    Update API base URL to use environment variable

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const API=axios.create({
-    baseURL:'http://localhost:5000/api',
+const API = axios.create({
+    baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : 'http://localhost:5000/api',
 });
 
-API.interceptors.request.use((config)=>{
-    const token=localStorage.getItem('token');
-    if(token){
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
-})
+});
 
 export default API;
