6f23248c095e3b8ccba295185af0c4ea3481dd26
krishnasairayavaram <krishnasairayavaram2005@gmail.com>
2026-02-12 13:25:53 +0530

    Replace axios with API for login request

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react";
-import axios from "axios";
+import API from "../api/axios";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate, Link } from "react-router-dom";
 
@@ -12,7 +12,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", { email, password });
+      const res = await API.post("/auth/login", { email, password });
       login(res.data.user, res.data.token);
       navigate("/");
     } catch (err) {
