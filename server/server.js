91a4cb8a588b0f1064c433037d314cc4d22594a2
krishnasairayavaram <krishnasairayavaram2005@gmail.com>
2026-02-12 12:27:06 +0530

    Restrict CORS to specific origin

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import paymentRoutes from "./routes/payment.routes.js";
 
 const app=express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://smart-winnr-event-booking-system.vercel.app" }));
 const MONGO_URL=process.env.MONGO_URL;
 const connectDB=async()=>{
     try{
